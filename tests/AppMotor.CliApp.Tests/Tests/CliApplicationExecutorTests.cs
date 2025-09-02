7463acd9d3c4fdd58b52c4b578de05112f0eb70d
Sebastian Krysmanski <noreply@manski.net>
2025-09-02 13:06:50 +0200

    Update repository url in generated .cs files

--- tests/AppMotor.CliApp.Tests/Tests/CliApplicationExecutorTests.cs
+++ tests/AppMotor.CliApp.Tests/Tests/CliApplicationExecutorTests.cs
@@ -4,7 +4,7 @@
 //
 
 // SPDX-License-Identifier: MIT
-// Copyright AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+// Copyright AppMotor Framework (https://github.com/skrysm/AppMotor)
 
 using AppMotor.CliApp.TestUtils;
 
