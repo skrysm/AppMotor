0b96e841d5155576bd9552b7d2f1374cd51cb5d7
Sebastian Krysmanski <noreply@manski.net>
2025-09-13 15:19:40 +0200

    Add DefaultNamespaceAttribute

---
+++ src/CoreKit/DefaultNamespaceAttribute.cs
@@ -0,0 +1,41 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright AppMotor Framework (https://github.com/skrysm/AppMotor)
+
+using AppMotor.CoreKit.Extensions;
+using AppMotor.CoreKit.Utils;
+
+using JetBrains.Annotations;
+
+namespace AppMotor.CoreKit;
+
+/// <summary>
+/// Stores the assembly's default namespace. The default namespace is used by default for EmbeddedResource names.
+/// </summary>
+/// <remarks>
+/// This attribute is automatically created for any assembly that directly or indirectly references
+/// the CoreKit during build (unless the <c>&lt;CreateDefaultNamespaceAttribute&gt;</c> property is
+/// set to <c>false</c>).
+/// </remarks>
+/// <remarks>
+/// In MSBuild, this value is called "root namespace" instead of "default namespace".
+/// </remarks>
+/// <seealso cref="AssemblyExtensions.GetDefaultNamespace"/>
+[AttributeUsage(AttributeTargets.Assembly)]
+public sealed class DefaultNamespaceAttribute : Attribute
+{
+    /// <summary>
+    /// The name of the default namespace (often the same as the assembly name).
+    /// </summary>
+    public string NamespaceName { get; }
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    [UsedImplicitly]
+    public DefaultNamespaceAttribute(string namespaceName)
+    {
+        Validate.ArgumentWithName(nameof(namespaceName)).IsNotNullOrWhiteSpace(namespaceName);
+
+        this.NamespaceName = namespaceName;
+    }
+}
---
+++ src/CoreKit/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,38 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright AppMotor Framework (https://github.com/skrysm/AppMotor)
+
+using System.Reflection;
+
+using JetBrains.Annotations;
+
+namespace AppMotor.CoreKit.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="Assembly"/>.
+/// </summary>
+public static class AssemblyExtensions
+{
+    /// <summary>
+    /// Returns the default namespace for the specified assembly.
+    /// </summary>
+    /// <remarks>
+    /// For this method to work, the default namespace must be stored in the <see cref="DefaultNamespaceAttribute"/>.
+    /// This attribute is automatically created for any assembly that directly or indirectly references
+    /// the CoreKit during build (unless the <c>&lt;CreateDefaultNamespaceAttribute&gt;</c> property is
+    /// set to <c>false</c>).
+    /// </remarks>
+    /// <exception cref="InvalidOperationException">Thrown if the default namespace hasn't been stored in the
+    /// <see cref="DefaultNamespaceAttribute"/>.</exception>
+    [PublicAPI, MustUseReturnValue]
+    public static string GetDefaultNamespace(this Assembly assembly)
+    {
+        var attribute = assembly.GetCustomAttribute<DefaultNamespaceAttribute>();
+
+        if (attribute is null)
+        {
+            throw new InvalidOperationException("The default namespace hasn't been stored in this assembly.");
+        }
+
+        return attribute.NamespaceName;
+    }
+}
